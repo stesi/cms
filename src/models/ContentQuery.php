2c57cdf1277f34f995ddc594f4f8626a8aa6467d
Mariangela <difazio.m@stesi.eu>
2017-10-05 10:05:07 +0200

    CMS

--- src/models/ContentQuery.php
+++ src/models/ContentQuery.php
@@ -42,4 +42,18 @@ class ContentQuery extends \yii\db\ActiveQuery
     public function getContent($content_type_id){
         return $this->andWhere(["content_type_id"=>$content_type_id]);
     }
+
+    /**
+     * Restituisce la lista dei content contenuti in un dato content padre, bisogna definire anche il numero degli elementi
+     * che saranno restituiti
+     * @return ActiveQuery
+     */
+    public function getContentChild($content_type_id, $content_parent_id, $limit){
+
+        return $this->getContent($content_type_id)
+            ->innerJoin("content_relation_manager", "content.id = content_relation_manager.content_child_id")
+            ->andWhere(["content_relation_manager.content_parent_id"=>$content_parent_id])
+            ->orderBy('position')
+            ->limit($limit);
+    }
 }
